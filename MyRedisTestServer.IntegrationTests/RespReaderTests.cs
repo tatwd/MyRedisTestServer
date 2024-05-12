5b4e30a681e2da80f019d6c01a1d94d6b7c81a09
tatwd <tatwdo@gmail.com>
2024-05-12 16:54:28 +0800

    Impl RespReader

---
+++ MyRedisTestServer.IntegrationTests/RespReaderTests.cs
@@ -0,0 +1,101 @@
+﻿namespace MyRedisTestServer.IntegrationTests;
+
+using static RespReader;
+using static RespTypeBuilder;
+
+public class RespReaderTests
+{
+    [Test]
+    public void ReadArray_ok()
+    {
+        var actual = ReadArray(Strings("foo", "bar"));
+        var expected = new[] { String("foo"), String("bar") };
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ReadString_ok()
+    {
+        var actual = ReadString(String("foo"));
+        Assert.That(actual, Is.EqualTo("foo"));
+    }
+    
+    [Test]
+    public void ReadStrings_ok()
+    {
+        var actual = ReadStrings(Strings("foo", "bar"));
+        var expected = new[] { "foo", "bar" };
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void Read_ok()
+    {
+        // blob strings
+        ReadTest("$11\r\nhello world\r\n");
+        ReadTest("$0\r\n\r\n");
+        
+        // simple strings
+        ReadTest("+abc\r\n");
+        ReadTest("+\r\n");
+        
+        // simple errors
+        ReadTest("-ERR wrong\r\n");
+        
+        // int
+        ReadTest(":10\r\n");
+        
+        // float
+        ReadTest(",10\r\n");
+        ReadTest(",10.0\r\n");
+        ReadTest(",10.123\r\n");
+        ReadTest(",inf\r\n");
+        ReadTest(",-inf\r\n");
+        
+        // null
+        ReadTest("_\r\n");
+        
+        // array
+        ReadTest("*0\r\n");
+        ReadTest("*1\r\n-foo\r\n");
+        ReadTest("*2\r\n-foo\r\n$3\r\nfoo\r\n");
+        ReadTest("*-1\r\n");
+        
+        // push
+        ReadTest(">0\r\n");
+        ReadTest(">1\r\n-foo\r\n");
+        ReadTest(">2\r\n-foo\r\n$3\r\nfoo\r\n");
+        ReadTest(">-1\r\n");
+        
+        // nil
+        ReadTest("$-1\r\n");
+        
+        // map
+        ReadTest("%0\r\n");
+        ReadTest("%1\r\n-foo\r\n-bar\r\n");
+        ReadTest("%2\r\n-foo\r\n$3\r\nfoo\r\n-bar\r\n-bar\r\n");
+        ReadTest("%-1\r\n");
+        
+        // set
+        ReadTest("~0\r\n");
+        ReadTest("~1\r\n-foo\r\n");
+        ReadTest("~2\r\n-foo\r\n$3\r\nfoo\r\n");
+        ReadTest("~-1\r\n");
+        
+    }
+
+    private static void ReadTest(string payload)
+    {
+        const string ping = "+ping\r\n";
+        using var r = new StringReader(payload + ping);
+        
+        var actual = Read(r);
+        
+        Assert.That(actual, Is.EqualTo(payload));
+
+        var buff = new char[7];
+        r.ReadBlock(buff, 0, 7);
+        
+        Assert.That(new string(buff), Is.EqualTo(ping));
+    }
+}
--- MyRedisTestServer/RespReader.cs
+++ MyRedisTestServer/RespReader.cs
@@ -8,58 +8,220 @@ using System.Threading.Tasks;
 
 namespace MyRedisTestServer;
 
-public struct Error
+public static class RespReader
 {
-    public readonly string type;
-    public readonly string message;
-
-    public Error(string type, string message)
+    private const string ErrProtocol = "unsupported protocol";
+    private const string ErrUnexpected = "not what you asked for";
+    
+    public static string[] ReadArray(string str)
     {
-        this.type = type.ToUpper();
-        this.message = message;
-    }
-}
+        using var stringReader = new StringReader(str);
 
+        var line = ReadLineInternal(stringReader);
 
-public struct RespResult
-{
-    public RespResultType Type { get; set; }
-    public object Result { get; set; }
+        var elems = 0;
+        
+        switch (line[0])
+        {
+            case '*': // array
+            case '>': // push data
+            case '~': // set
+                elems = ReadLength(line);
+                break;
+            case '%': // maps
+                var length2 = ReadLength(line);
+                elems = length2 * 2;
+                break;
+            
+            default:
+                throw new NotSupportedException(ErrProtocol);
+        }
 
-    public RespResult(RespResultType type, object result)
-    {
-        Type = type;
-        Result = result;
-    }
-}
-
-public enum RespResultType
-{
-    String = 0,
-    Error = 1,
-    Number = 2,
-    Strings = 3,
-    Array = 4,
-}
-
-
-
-public class RespReader
-{
-    private TextReader reader;
-
-    public RespReader(string str)
-    {
-        reader = new StringReader(str);
+        var res = new string[elems];
+        for (var i = 0; i < elems; i++)
+        {
+            var next = Read(stringReader);
+            res[i] = next;
+        }
+        
+        return res;
     }
 
-    public RespReader(Stream stream)
+    public static string ReadString(string str)
     {
-        reader = new StreamReader(stream);
+        using var stringReader = new StringReader(str);
+        var line = ReadLineInternal(stringReader);
+
+        switch (line[0])
+        {
+            case '$':
+                var length1 = ReadLength(line);
+                if (length1 < 0)
+                {
+                    // -1 is a nil response
+                    return line;
+                }
+                
+                var buff = ReadChars(stringReader, length1);
+                return new string(buff, 0, buff.Length - 2);
+                
+            default:
+                throw new NotSupportedException(ErrProtocol);
+        }
+        
     }
 
-    public RespResult Read()
+    public static string[]? ReadStrings(string str)
     {
-        throw new NotImplementedException();
+        var items = ReadArray(str);
+        
+        var res = new string[items.Length];
+        for (var i = 0; i < items.Length; i++)
+        {
+            res[i] = ReadString(items[i]);
+        }
+
+        return res;
     }
+    
+    // Read a single command, returning it raw. Used to read replies from redis.
+    // Understands RESP3 proto.
+    public static string Read(TextReader textReader)
+    {
+        var line = ReadLineInternal(textReader);
+
+        switch (line[0])
+        {
+            case '+': // inline string
+            case '-': // error
+            case ':': // int
+            case ',': //float
+            case '_': //null
+                return line;
+
+            case '$':
+            {
+                // bulk strings are: `$5\r\nhello\r\n`
+                var length = ReadLength(line);
+                if (length < 0)
+                {
+                    // -1 is a nil response
+                    return line;
+                }
+
+                var buff = ReadChars(textReader, length);
+                return line + new string(buff);
+            }
+            case '*': // arrays are: `*6\r\n...`
+            case '>': // pushdata is: `>6\r\n...`
+            case '~': // sets are: `~6\r\n...`
+            {
+                var length = ReadLength(line);
+
+                for (var i = 0; i < length; i++)
+                {
+                    var next = Read(textReader);
+                    line += next;
+                }
+
+                return line;
+            }
+            case '%':
+            {
+                var length = ReadLength(line);
+                for (var i = 0; i < length * 2; i++)
+                {
+                    var next = Read(textReader);
+                    line += next;
+                }
+                return line;
+            }
+            
+            default:
+                throw new NotSupportedException(ErrProtocol);
+        }
+    }
+
+    public static string ReadError(string str)
+    {
+        if (str.Length < 1 || str[0] != '-')
+        {
+            throw new ArgumentException(ErrUnexpected);
+        }
+
+        return ReadInline(str);
+    }
+    
+
+    private static string ReadInline(string str)
+    {
+        if (str.Length < 3)
+        {
+            throw new ArgumentException(ErrUnexpected);
+        }
+
+        return str.Substring(1, str.Length - 2);
+    }
+    
+    private static int ReadLength(string line)
+    {
+        return int.Parse(line.Substring(1, line.Length - 2));
+    }
+
+    private static char[] ReadChars(TextReader textReader, int length)
+    {
+        var pos = 0;
+        var buff = new char[length + 2];
+        
+        while (pos < length + 2)
+        {
+            var n = textReader.Read(buff, pos, buff.Length);
+            pos += n;
+        }
+
+        return buff;
+    }
+
+    private static string ReadLineInternal(TextReader textReader)
+    {
+        var line = new StringBuilder(128);
+        
+        var hasNext = TryReadNextChar(textReader, out var next);
+
+        while (hasNext)
+        {
+            line.Append(next);
+
+            if (next == '\n')
+            {
+                break;
+            }
+            
+            hasNext = TryReadNextChar(textReader, out next);
+        }
+        
+        if (line.Length < 3)
+        {
+            throw new NotSupportedException(ErrProtocol);
+        }
+
+        return line.ToString();
+    }
+
+    private static bool TryReadNextChar(TextReader reader, out char next)
+    {
+        next = default;
+        var c = reader.Read();
+
+        if (c == -1)
+        {
+            return false;
+        }
+        
+        next = (char)c;
+        return true;
+
+    }
+    
+    
 }
