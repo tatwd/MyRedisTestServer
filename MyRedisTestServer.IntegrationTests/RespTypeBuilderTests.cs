6b4201a598e45840e3b2a2cd06f3dd4cdcbb041c
tatwd <tatwdo@gmail.com>
2024-05-12 14:25:50 +0800

    Add RespTypeBuilder

---
+++ MyRedisTestServer.IntegrationTests/RespTypeBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿namespace MyRedisTestServer.IntegrationTests;
+
+using static RespTypeBuilder;
+
+public class RespTypeBuilderTests
+{
+    [Test]
+    public void BuildRespTypes_ok()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(String(null), Is.EqualTo("$0\r\n\r\n"));
+            Assert.That(String(""), Is.EqualTo("$0\r\n\r\n"));
+            Assert.That(String("foo"), Is.EqualTo("$3\r\nfoo\r\n"));
+
+            Assert.That(Inline("Hi"), Is.EqualTo("+Hi\r\n"));
+
+            Assert.That(Error("ERR wrong"), Is.EqualTo("-ERR wrong\r\n"));
+
+            Assert.That(Int(12), Is.EqualTo(":12\r\n"));
+
+            Assert.That(Float(12.13), Is.EqualTo(",12.13\r\n"));
+            
+            Assert.That(Array(Inline("hi"), Inline("ho")), Is.EqualTo("*2\r\n+hi\r\n+ho\r\n"));
+            Assert.That(Strings("hi", "ho"), Is.EqualTo("*2\r\n$2\r\nhi\r\n$2\r\nho\r\n"));
+
+            Assert.That(Push(Inline("hi"), Inline("ho")), Is.EqualTo(">2\r\n+hi\r\n+ho\r\n"));
+            
+            Assert.That(Map(String("hi"), String("ho")), Is.EqualTo("%1\r\n$2\r\nhi\r\n$2\r\nho\r\n"));
+            Assert.That(StringMap("hi", "ho"), Is.EqualTo("%1\r\n$2\r\nhi\r\n$2\r\nho\r\n"));
+            
+            
+            Assert.That(Set(String("hi"), String("ho")), Is.EqualTo("~2\r\n$2\r\nhi\r\n$2\r\nho\r\n"));
+            Assert.That(StringSet("hi", "ho"), Is.EqualTo("~2\r\n$2\r\nhi\r\n$2\r\nho\r\n"));
+            
+        });
+    }
+    
+}
---
+++ MyRedisTestServer/RespTypeBuilder.cs
@@ -0,0 +1,107 @@
+﻿namespace MyRedisTestServer;
+
+public static class RespTypeBuilder
+{
+    public static string String(string? str)
+    {
+        return $"${str?.Length ?? 0}\r\n{str}\r\n";
+    }
+
+    public static string Inline(string str)
+    {
+        return $"+{str}\r\n";
+    }
+
+    public static string Error(string err)
+    {
+        return $"-{err}\r\n";
+    }
+
+    public static string Int(int n)
+    {
+        return $":{n}\r\n";
+    }
+
+    public static string Float(double n)
+    {
+        return $",{n}\r\n";
+    }
+
+    public static string Nil() => "$-1\r\n";
+
+    public static string NilResp3() => "_\r\n";
+    
+    public static string NilList() => "*-1\r\n";
+
+    // Array(true, String("foo"), String("bar"))
+    public static string Array(params string[] args)
+    {
+        return $"*{args.Length}\r\n" + string.Join("", args);
+    }
+    
+    public static string Push(params string[] args)
+    {
+        return $">{args.Length}\r\n" + string.Join("", args);
+    }
+
+    public static string Strings(params string[] args)
+    {
+        var arr = new string[args.Length];
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            arr[i] = String(args[i]);
+        }
+
+        return Array(arr);
+    }
+
+    public static string Ints(params int[] args)
+    {
+        var arr = new string[args.Length];
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            arr[i] = Int(args[i]);
+        }
+
+        return Array(arr);
+    }
+
+    // Map(String("foo"), String("bar"))
+    public static string Map(params string[] args)
+    {
+        return $"%{args.Length / 2}\r\n" + string.Join("", args);
+    }
+
+    public static string StringMap(params string[] args)
+    {
+        var arr = new string[args.Length];
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            arr[i] = String(args[i]);
+        }
+
+        return Map(arr);
+    }
+    
+    // Set(String("foo"), String("bar"))
+    public static string Set(params string[] args)
+    {
+        return $"~{args.Length}\r\n" + string.Join("", args);
+    }
+
+    public static string StringSet(params string[] args)
+    {
+        var arr = new string[args.Length];
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            arr[i] = String(args[i]);
+        }
+
+        return Set(arr);
+    }
+
+}
